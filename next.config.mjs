d6956013fd6cfa0cb7f3cf86179852a4b77a7c92
bandfk913-ai <bandfk913@gmail.com>
2025-12-31 20:51:38 -0500

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // We removed "output: standalone" to fix the Railway conflict
   images: {
     unoptimized: true,
   },
 };
+
 export default nextConfig;
